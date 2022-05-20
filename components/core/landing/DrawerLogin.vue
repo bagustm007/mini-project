679b9b341e01b10ee50681ff3041c850bdcb47ed
bagustm007 <105295387+bagustm007@users.noreply.github.com>
2022-05-20 17:55:00 +0700

    test

---
+++ components/core/landing/DrawerLogin.vue
@@ -0,0 +1,66 @@
+<template>
+  <v-navigation-drawer 
+    absolute
+    right
+    app
+    v-model="inputValue"
+    mobile-breakpoint="911"
+    persistent
+    width="260"
+  >
+    <v-list 
+    class="pl-15 mt-10"
+    shaped
+    >
+        <v-list-item link to="/">
+            <v-list-item-content>
+                <v-list-item-title class="link--text ">
+                    Beranda
+                </v-list-item-title>
+            </v-list-item-content>
+        </v-list-item>
+     <v-list-item link to="/about">
+            <v-list-item-content>
+                <v-list-item-title class="link--text ">
+                    Tentang Aplikasi
+                </v-list-item-title>
+            </v-list-item-content>
+        </v-list-item>     
+
+    </v-list>
+  </v-navigation-drawer>
+</template>
+
+<script>
+import {mapMutations, mapState} from 'vuex';
+
+export default {
+    props: {
+        opened: {
+            type: Boolean,
+            default: false,
+        }
+    },
+
+        computed: {
+        inputValue: {
+            get(){
+                return this.$store.state.app.draweLanding;
+            },
+            set(val){
+                this.setDraweLanding(val);
+            },
+        }
+    },
+    
+    methods: {
+        ...mapMutations('app', ['setDraweLanding', 'toggleDrawerLanding']),
+    }
+}
+</script>
+
+<style>
+    .link-pointer{
+        cursor: pointer !important;
+    }
+</style>
